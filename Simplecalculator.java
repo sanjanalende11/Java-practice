2fa547e4b55352fe86a62c46a80587db4717b79c
Sanjana Lende <sanjanalende127@gmail.com>
2025-12-18 15:09:12 +0530

    Create Simplecalculator.java

---
+++ Simplecalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+                           }
